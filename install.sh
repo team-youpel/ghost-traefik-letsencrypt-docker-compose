2eb57db5045264674ba13cf9a80f3b1af700b996
team-youpel <77631046+team-youpel@users.noreply.github.com>
2024-10-24 18:23:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -133,7 +133,7 @@ EOF
     docker network create ghost-network
 
     # Start the Docker containers
-    docker compose -f ghost-traefik-letsencrypt-docker-compose.yml -p ghost up
+    docker compose -f ghost-traefik-letsencrypt-docker-compose.yml -p ghost up -d
 
     echo -e "${BOLDGREEN}✔ Ghost blog should now be accessible after a few seconds at https://$domain_name${ENDCOLOR}"
 else
